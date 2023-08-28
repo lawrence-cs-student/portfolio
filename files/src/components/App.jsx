8f13d3045146d959c364df4ca5e3ed3caa8c3b8e
lawrence-cs-student <114126573+lawrence-cs-student@users.noreply.github.com>
2023-08-28 22:05:16 +0800

    Update App.jsx

--- files/src/components/App.jsx
+++ files/src/components/App.jsx
@@ -14,7 +14,7 @@ export default function App () {
             </div>
             <Skills />
             <Projects />
-            {/* <Footer /> */}
+            <Footer />
         </div>
     )
 }
